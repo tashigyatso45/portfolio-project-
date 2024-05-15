7051f5afd667f606f8db877d7e8681f19f7e4ad6
tashigyatso <tashigyatso45@gmail.com>
2024-05-14 23:03:49 -0400

    back on track

--- real-portfolio-project/src/App.jsx
+++ real-portfolio-project/src/App.jsx
@@ -4,7 +4,6 @@ import Navbar from './Components/Navbar/Navbar'
 function App() {
   return (
     <div>
-      ssss
       <Navbar/>
     </div>
   )
--- real-portfolio-project/src/Components/Navbar/Navbar.jsx
+++ real-portfolio-project/src/Components/Navbar/Navbar.jsx
@@ -10,6 +10,8 @@ const Navbar = () => {
           <li>Services</li>
           <li>Portfolio</li>
           <li>Contact</li>
+         
+      
       </ul>
       <div className="nav-connect">Connect With Me</div>
 
