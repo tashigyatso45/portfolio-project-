8ec4da2b111a4b9c21188e71021c4ac2b2006130
tashigyatso <tashigyatso45@gmail.com>
2024-05-14 22:41:25 -0400

    Add a logo image and nav- menu

--- real-portfolio-project/src/Components/Navbar/Navbar.jsx
+++ real-portfolio-project/src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,19 @@ import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+        <img src={img} alt="" />
+        <ul className="nav-menu">
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+        </ul>
+        <div className="nav-connect">Connect With Me</div>
+
+    </div>
+
   )
 }
 
