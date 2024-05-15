403dc00c8943dc65bd0664c7b81cef6af9f9b94f
tashigyatso <tashigyatso45@gmail.com>
2024-05-14 22:54:25 -0400

    git commit -m " Fixed bug regarding img tag"

--- real-portfolio-project/src/Components/Navbar/Navbar.jsx
+++ real-portfolio-project/src/Components/Navbar/Navbar.jsx
@@ -4,15 +4,14 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <img src={img} alt="" />
-        <ul className="nav-menu">
-            <li>Home</li>
-            <li>About Me</li>
-            <li>Services</li>
-            <li>Portfolio</li>
-            <li>Contact</li>
-        </ul>
-        <div className="nav-connect">Connect With Me</div>
+      <ul className="nav-menu">
+          <li>Home</li>
+          <li>About Me</li>
+          <li>Services</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+      </ul>
+      <div className="nav-connect">Connect With Me</div>
 
     </div>
 
