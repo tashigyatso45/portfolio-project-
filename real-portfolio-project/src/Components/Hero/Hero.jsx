19115822ecf43fafd0ee353978c2d2b3777a32a9
tashigyatso <tashigyatso45@gmail.com>
2024-05-16 16:48:45 -0400

    more styling for the hero component

--- real-portfolio-project/src/Components/Hero/Hero.jsx
+++ real-portfolio-project/src/Components/Hero/Hero.jsx
@@ -1,19 +1,20 @@
-import './Hero.css'
-import profile from'../../assets/picture.png'
+import "./Hero.css";
+import profile from "../../assets/about_profile.png";
 
 const Hero = () => {
   return (
-    <div className='hero'>
-        <img src={profile} alt="" className='profile-picture'/>
-        <h1> I am Tashi, frontend developer  </h1>
-        <p>I am a front end developer from NYC </p>
-        <div className="hero-action">
-            <div className="hero-conect"> connect with me </div>
-            <div className="hero-resume">My Resume </div>
-        </div>
-
+    <div className="hero">
+      <img src={profile} alt="" className="profile-picture" />
+      <h1>
+        <span>I am Tashi,</span> frontend developer
+      </h1>
+      <p>I am a front end developer from NYC </p>
+      <div className="hero-action">
+        <div className="hero-connect"> connect with me </div>
+        <div className="hero-resume">My Resume </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
