3355ba1fbe0237b71b5b8a919d5f2c29343e6ae2
tashigyatso <tashigyatso45@gmail.com>
2024-09-24 13:08:09 -0400

    Created a footer componet folder with jsx and css file

--- real-portfolio-project/src/App.jsx
+++ real-portfolio-project/src/App.jsx
@@ -1,6 +1,7 @@
 
 import About from './Components/About/About'
 import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
 import Hero from './Components/Hero/Hero'
 import MyWork from './Components/MyWork/MyWork'
 import Navbar from './Components/Navbar/Navbar'
@@ -15,6 +16,7 @@ function App() {
       <Services/>
       <MyWork/>
       <Contact/>
+      <Footer/>
     </div>
   )
 }
