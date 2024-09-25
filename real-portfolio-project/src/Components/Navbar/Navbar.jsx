84bd8443cf54d818e20345122e6ef99309dc8302
tashigyatso <tashigyatso45@gmail.com>
2024-09-25 09:43:58 -0400

    more functionality

--- real-portfolio-project/src/Components/Navbar/Navbar.jsx
+++ real-portfolio-project/src/Components/Navbar/Navbar.jsx
@@ -6,16 +6,24 @@ import underline from '../../assets/nav_underline.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import menu_open from '../../assets/menu_open.svg'
 import menu_close from '../../assets/menu_close.svg'
-
+import { useRef } from 'react'
 const Navbar = () => {
 
-const [menu, setMenu] = useState("home")
+const [menu, setMenu] = useState("home");
+const menuRef = useRef();
+
+const openMenu = () => {
+  menuRef.current.style.right = '0'
+}
+const closeMenu = () => {
+  menuRef.current.style.right = '-350px'
+}
   return (
     <div className='navbar'>
       <img className='logo' src={logo} alt="" />
-      <img src={menu_open} alt="" className='nav-mob-open'/>
+      <img src={menu_open} onClick={openMenu} alt="" className='nav-mob-open'/>
       <ul className="nav-menu">
-          <img className=" nav-mob-close" src={menu_close} alt="" />
+          <img className=" nav-mob-close" src={menu_close} onClick={closeMenu} alt="" />
           <li> <AnchorLink className='anchor-link'  href='#home'><p onClick={()=> setMenu("home")}>Home</p></AnchorLink>{menu === "home" ? <img src = {underline}/> : <></>}</li>
           <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={()=> setMenu("about")}>About Me</p></AnchorLink>{menu === "about" ? <img src = {underline}/> : <></>}</li>
           <li><AnchorLink className='anchor-link' offset={50} href='#services' ><p onClick={()=> setMenu("services")}>Services</p></AnchorLink>{menu === "services" ? <img src = {underline}/> : <></>}</li>
